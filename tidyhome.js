951841a94db7c1def644c1b20a609013fdd519de
James Kim <jameskim1093@gmail.com>
2016-03-03 15:33:53 -0800

    update

---
+++ tidyhome.js
@@ -0,0 +1,10 @@
+$.backstretch([
+        "images/house.jpg",
+          "images/water.jpg",
+          "images/lights.JPG",
+          "images/subway.jpg",
+          "images/park.JPG"
+        ], {
+            fade: 750,
+            duration: 4000
+        });

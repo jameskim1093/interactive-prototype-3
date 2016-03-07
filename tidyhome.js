6d5d9ce6362ca23fef078f5686954912ea75f40a
James Kim <jameskim1093@gmail.com>
2016-03-06 22:36:33 -0800

    initial commit

--- tidyhome.js
+++ tidyhome.js
@@ -1,10 +1,29 @@
-$.backstretch([
-        "images/house.jpg",
-          "images/water.jpg",
-          "images/lights.JPG",
-          "images/subway.jpg",
-          "images/park.JPG"
-        ], {
-            fade: 750,
-            duration: 4000
-        });
+$(document).ready(function() {
+  console.log("ready");
+    
+$('section#sign-up-screen').hide();
+                  
+$('#sign-up').click(function(){
+    $('#home-screen').fadeOut(); 
+  });
+
+$('#sign-up').click(function(){
+    $('#sign-up-screen').fadeIn();
+});
+
+$('.log-btn').click(function(){
+    $('#home-screen').fadeOut(); 
+  });
+
+$('.log-btn').click(function(){
+    $('#main-screen').fadeIn(); 
+  });
+
+$('#submit-btn').click(function(){
+    $('#sign-up-screen').fadeOut();
+});
+    
+$('#submit-btn').click(function(){
+    $('#main-screen').fadeIn(); 
+  });
+});

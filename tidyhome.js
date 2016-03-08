0c4f011b5dbc46be78b2e43b95ff1bf8860240a6
James Kim <jameskim1093@gmail.com>
2016-03-07 23:42:41 -0800

    update

--- tidyhome.js
+++ tidyhome.js
@@ -1,11 +1,7 @@
 $(document).ready(function() {
   console.log("ready");
     
-//$('section#home-screen').hide();    
 $('section#sign-up-screen').hide();
-$('section#main-screen').hide();
-$('section#sidebar').hide();
-$('section#chore-form').hide();
                   
 $('#sign-up').click(function(){
     $('#home-screen').fadeOut(); 
@@ -16,55 +12,18 @@ $('#sign-up').click(function(){
 });
 
 $('.log-btn').click(function(){
-    $('#home-screen').fadeOut();
-    $('#home-page').fadeOut();
+    $('#home-screen').fadeOut(); 
   });
 
 $('.log-btn').click(function(){
     $('#main-screen').fadeIn(); 
   });
 
-$('#submit-btn-1').click(function(){
+$('#submit-btn').click(function(){
     $('#sign-up-screen').fadeOut();
-    $('#home-page').fadeOut();
 });
     
-$('#submit-btn-1').click(function(){
+$('#submit-btn').click(function(){
     $('#main-screen').fadeIn(); 
   });
-    
-$('.list-icon').click(function(){
-    $('#sidebar').fadeIn();
-    $('#sidebar').show().css('position', 'absolute');
-    $('.add-chore-list').hide();
-    $('.add-icon-large').hide();
 });
-    
-$('.back-icon').click(function(){
-    $('#sidebar').fadeOut();
-    $('.add-chore-list').show();
-    $('.add-icon-large').show();
-});
-
-$('.add-chore-list').click(function(){
-    $('#chore-form').fadeIn();
-    $('.add-chore-list').hide();
-    $('.add-icon-large').hide();
-});
-
-$('.add-icon-large').click(function(){
-    $('#chore-form').fadeIn();
-    $('.add-chore-list').hide();
-    $('.add-icon-large').hide();
-});
-   
-$('#submit-btn-2').click(function(){
-    $('#chore-form').fadeOut();
-});
-    
-$('#submit-btn-2').click(function(){
-    $('#main-screen').fadeIn(); 
-});
-});
-    
-
